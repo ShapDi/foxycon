b064350287304d8a05a3bc623d0a9393ef8b2a45
ROCNCODER <85634219+ROCNCODER@users.noreply.github.com>
2024-09-30 02:04:30 +0300

    exp setup

--- setup.py
+++ setup.py
@@ -10,27 +10,8 @@ setup(
     author_email='shapranov.work@gmail.com',
     url='git@github.com:ShapDi/foxycon.git',
     install_requires=[
-        "aiohttp==3.9.2",
-        "aiosignal==1.3.1",
-        "cffi==1.16.0",
-        "charset-normalizer==2.1.1",
-        "frozenlist==1.3.3",
-        "idna==3.7",
-        "instagram-auth==0.1.1",
-        "instagram-reels @ git+ssh://git@github.com/technology-department-mb/instastat.git@6ece09397445fc78790aeb6072cc72cd1fa5a343"
-        "multidict==6.0.4",
-        "pycparser==2.21",
-        "pycryptodomex==3.19.1",
-        "PyNaCl==1.5.0",
         "pytube==15.0.0",
-        "regex==2024.9.11",
-        "requests==2.32.3",
-        "setuptools==68.2.0",
-        "soupsieve==2.3.2.post1",
-        "typing_extensions==4.12.2",
-        "urllib3==2.2.3",
-        "wheel==0.41.2",
-        "yarl==1.8.2"
+        "instagram-reels @ git+ssh://git@github.com/technology-department-mb/instastat.git@6ece09397445fc78790aeb6072cc72cd1fa5a343"
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
