2e35959ab704457086c8cf44c78fa6274dd9479d
ROCNCODER <85634219+ROCNCODER@users.noreply.github.com>
2024-09-30 02:07:59 +0300

    exp setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='A package created to interact with various electrical networks based on other libraries.',
     author='ShapDi',
     packages=['foxycon', 'foxycon.analysis_services', 'foxycon.analysis_services', 'foxycon.data_structures',
-              'foxycon.statistics_services'],
+              'foxycon.statistics_services', 'foxycon.utils'],
     author_email='shapranov.work@gmail.com',
     url='git@github.com:ShapDi/foxycon.git',
     install_requires=[
